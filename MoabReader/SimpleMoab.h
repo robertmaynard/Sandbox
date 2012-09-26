2b78c529b4f3e8ab3c4c941e8cd59e6d45b68ea6
Robert Maynard <robert.maynard@kitware.com>
2012-09-26 10:40:21 -0400

    Update the simplified moab header.

--- MoabReader/SimpleMoab.h
+++ MoabReader/SimpleMoab.h
@@ -1,26 +1,21 @@
 
-#ifndef _tags_h
-#define _tags_h
+#ifndef _SimpleMoab_h
+#define _SimpleMoab_h
 
 #include "moab/Core.hpp"
-#include "moab/Interface.hpp"
+#include "moab/this->Moab.hpp"
 #include "moab/Range.hpp"
 
 #include <iostream>
-#include <memory>
 
-//light weight wrapper on a moab interface to explicitly manage
-//memory.
-struct Interface
+namespace smoab
 {
-  Interface():Interface_(new moab::Core()){}
-  ~Interface(){delete this->Interface_;}
+//make our range equal to moabs range
+typedef moab::Range Range;
+typedef moab::EntityHandle EntityHandle;
 
-  moab::Interface* operator->() const throw() { return this->Interface_; }
-private:
-  moab::Interface* Interface_;
-};
-
+//forward declare this->Moab for Tag
+struct Interface;
 
 struct Tag
 {
@@ -30,25 +25,19 @@ struct Tag
 
   Tag(Interface const& interface, std::string const& n):Name_(n),Value_(-1)
     {
-    interface->tag_get_handle(n.c_str(),1,moab::MB_TYPE_INTEGER,this->MTag_);
+    this->Moab->tag_get_handle(n.c_str(),1,moab::MB_TYPE_INTEGER,this->MTag_);
     }
 
   Tag(Interface const& interface, std::string const& n, int v):Name_(n),Value_(v)
     {
-    interface->tag_get_handle(n.c_str(),1,moab::MB_TYPE_INTEGER,this->MTag_);
+    this->Moab->tag_get_handle(n.c_str(),1,moab::MB_TYPE_INTEGER,this->MTag_);
     }
 
   const char* name() const { return this->Name_.c_str(); }
 
-  bool hasTagValue() const { return Value_ >= 0; }
-  bool tagValue() const { return Value_; }
   bool tagValuesMatch(int otherTagValue) const
    {
-   if(!hasTagValue())
-     {
-     return true;
-     }
-   return (otherTagValue == Value_);
+   return this->Value_ < 0 || (this->Value_ >= 0 && otherTagValue == Value_);
    }
 
   moab::Tag moabTag() const { return this->MTag_; }
@@ -56,112 +45,156 @@ struct Tag
 
 //lightweight structs to wrap set names, so we detected
 //incorrect names at compile time
-struct MaterialTag : Tag{ MaterialTag(Interface const& interface):Tag(interface,"MATERIAL_SET"){}};
-struct DirichletTag : Tag{ DirichletTag(Interface const& interface):Tag(interface,"DIRICHLET_SET"){}};
-struct NeumannTag: Tag{ NeumannTag(Interface const& interface):Tag(interface,"NEUMANN_SET"){}};
-struct GroupTag: Tag{ GroupTag(Interface const& interface):Tag(interface,"GROUP"){}};
-struct GeomTag: Tag{ GeomTag(Interface const& interface, int dim):Tag(interface,"GEOM_DIMENSION",dim){}};
+struct MaterialTag : Tag{ MaterialTag(smoab::Interface const& interface):Tag(interface,"MATERIAL_SET"){}};
+struct DirichletTag : Tag{ DirichletTag(smoab::Interface const& interface):Tag(interface,"DIRICHLET_SET"){}};
+struct NeumannTag: Tag{ NeumannTag(smoab::Interface const& interface):Tag(interface,"NEUMANN_SET"){}};
+struct GroupTag: Tag{ GroupTag(smoab::Interface const& interface):Tag(interface,"GROUP"){}};
+struct GeomTag: Tag{ GeomTag(smoab::Interface const& interface, int dim):Tag(interface,"GEOM_DIMENSION",dim){}};
 
 
-//coreate a moab interface to use
-bool LoadMoab(std::string const& file, Interface const& interface)
+//light weight wrapper on a moab this->Moab that exposes only the reduced class
+//that we need
+struct Interface
 {
-  moab::ErrorCode rval = interface->load_file(file.c_str());
-  return moab::MB_SUCCESS == rval;
-}
-
-
-//print all elements in a range
-size_t printRange(moab::Range const& range, const Interface& interface)
-{
-  typedef moab::Range::iterator iterator;
-  for(iterator i=range.begin(); i!=range.end(); ++i)
+  Interface(const std::string &file):
+    Moab(new moab::Core())
     {
-    std::cout << "entity id: " << *i << std::endl;
-    interface->list_entity(*i);
+    this->Moab->load_file(file.c_str());
     }
 
-  std::cout << "Printed: " << range.size() << std::endl;
-  return range.size();
-}
+  ~Interface(){delete interface;}
 
 
-//print all the entities with a give tag, be it material, boundary condition, geometery dim
-void findEntitiesWithTag(const Tag& tag, const Interface& interface, moab::EntityHandle root, moab::Range& entities)
-{
+  smoab::EntityHandle getRoot() const { return this->Moab->get_root_set(); }
 
-  moab::Tag t = tag.moabTag();
-  moab::Range beforeTagMatches;
-  // get all the sets of that type in the mesh
-  interface->get_entities_by_type_and_tag(root, moab::MBENTITYSET, &t,
-                                          NULL, 1, beforeTagMatches);
-
-  //now we have to remove any that doesn't match the tag value
-  typedef moab::Range::iterator iterator;
-  for(iterator i=beforeTagMatches.begin();
-      i != beforeTagMatches.end();
-      ++i)
+  smoab::Range findEntities(const smoab::EntityHandle root, moab::EntityType type) const
     {
-    int tagValue=0;
-    moab::EntityHandle handle = *i;
-    interface->tag_get_data(tag.moabTag(), &handle, 1, &tagValue);
-    if(tag.tagValuesMatch(tagValue))
-      {
-      entities.insert(*i);
-      }
+    smoab::Range result;
+    // get all the sets of that type in the mesh
+    this->Moab->get_entities_by_type(root, type, result);
+    return result;
     }
-}
 
-//an attempt to find all parents in the database that have children
-void findParents(const Interface& interface, moab::EntityHandle const& entity, moab::Range& parents)
-{
-  typedef moab::Range::iterator iterator;
-  moab::Range sets;
-  moab::ErrorCode rval;
-
-  rval = interface->get_entities_by_type(entity, moab::MBENTITYSET, sets);
-  if(moab::MB_SUCCESS != rval || sets.empty()) return;
-
-  for(iterator i=sets.begin(); i!=sets.end();++i)
+  //Find all entities with a given tag. If the tag has the optional value
+  //like GeomTag we will only match entities that have that value too.
+  //Returns the found group of entities
+  smoab::Range findEntitiesWithTag (const smoab::Tag& tag, smoab::EntityHandle root,
+                                    moab::EntityType type = moab::MBENTITYSET) const
     {
-    int numParents=0,numChildren=0;
-    interface->num_parent_meshsets(*i,&numParents);
-    if(numParents==0)
+    smoab::Range result;
+    moab::Range beforeTagMatches;
+
+    moab::Tag t = tag.moabTag();
+    // get all the sets of that type in the mesh
+    this->Moab->get_entities_by_type_and_tag(root, type, &t, NULL, 1,
+                                             beforeTagMatches);
+
+    //now we have to remove any that doesn't match the tag value
+    typedef moab::Range::iterator iterator;
+    for(iterator i=beforeTagMatches.begin();
+        i != beforeTagMatches.end();
+        ++i)
       {
-      interface->num_child_meshsets(*i,&numChildren);
-      if(numChildren>0)
+      int tagValue=0;
+      moab::EntityHandle handle = *i;
+      this->Moab->tag_get_data(tag.moabTag(), &handle, 1, &tagValue);
+      if(tag.tagValuesMatch(tagValue))
         {
-        parents.insert(*i);
+        result.insert(*i);
         }
       }
+
+    return result;
     }
-}
 
-//an attempt to find children with multiple parents of a given tag type
-void findEntitiesWithMultipleParents(const Interface& interface,
-                                     moab::Range const& range,
-                                     moab::Range& multipleParents)
-{
-  typedef moab::Range::iterator iterator;
+  //Find all elements in the database that have children and zero parents.
+  //this doesn't find
+  smoab::Range findEntityRootParents(smoab::EntityHandle const& root) const
+  {
+    smoab::Range parents;
 
-  //for all the elements in the range, find all items with multiple parents
+    typedef moab::Range::iterator iterator;
+    moab::Range sets;
 
-  for(iterator i=range.begin(); i!=range.end(); ++i)
+    this->Moab->get_entities_by_type(root, moab::MBENTITYSET, sets);
+    for(iterator i=sets.begin(); i!=sets.end();++i)
+      {
+      int numParents=0,numChildren=0;
+      this->Moab->num_parent_meshsets(*i,&numParents);
+      if(numParents==0)
+        {
+        this->Moab->num_child_meshsets(*i,&numChildren);
+        if(numChildren>0)
+          {
+          parents.insert(*i);
+          }
+        }
+      }
+    return parents;
+  }
+
+  //finds entities that have zero children and zero parents
+  smoab::Range findDetachedEntities(moab::EntityHandle const& root) const
     {
-    moab::Range children;
-    interface->get_child_meshsets(*i,children,0);
+    smoab::Range detached;
 
-    for(iterator j=children.begin(); j!=children.end();++j)
+    typedef moab::Range::iterator iterator;
+    moab::Range sets;
+
+    this->Moab->get_entities_by_type(root, moab::MBENTITYSET, sets);
+    for(iterator i=sets.begin(); i!=sets.end();++i)
+      {
+      int numParents=0,numChildren=0;
+      this->Moab->num_parent_meshsets(*i,&numParents);
+      if(numParents==0)
+        {
+        this->Moab->num_child_meshsets(*i,&numChildren);
+        if(numChildren==0)
+          {
+          detached.insert(*i);
+          }
+        }
+      }
+    return detached;
+    }
+
+  //find all children of the entity passed in that has multiple parents
+  smoab::Range findEntitiesWithMultipleParents(smoab::EntityHandle const& root)
+  {
+    smoab::Range multipleParents;
+    typedef moab::Range::iterator iterator;
+
+    //for all the elements in the range, find all items with multiple parents
+    moab::Range children;
+    this->Moab->get_child_meshsets(root,children,0);
+    for(iterator i=children.begin(); i!=children.end();++i)
       {
       int numParents=0;
-      interface->num_parent_meshsets(*j,&numParents);
+      this->Moab->num_parent_meshsets(*i,&numParents);
       if(numParents>1)
         {
-        multipleParents.insert(*j);
+        multipleParents.insert(*i);
         }
       }
-    }
-}
+    return multipleParents;
+  }
 
+
+  //prints all elements in a range objects
+  void printRange(smoab::Range const& range)
+    {
+    typedef Range::iterator iterator;
+    for(iterator i=range.begin(); i!=range.end(); ++i)
+      {
+      std::cout << "entity id: " << *i << std::endl;
+      this->Moab->list_entity(*i);
+      }
+    }
+
+private:
+  moab::Interface* Moab;
+};
+
+}
 
 #endif
--- MoabReader/vtkMoabReader.cxx
+++ MoabReader/vtkMoabReader.cxx
@@ -1,7 +1,46 @@
-#include "vtkmoabreader.h"
+#include "vtkMoabReader.h"
 
-#include "tags.h"
+#include "SimpleMoab.h"
 
+#include "vtkObjectFactory.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkMultiBlockDataSet.h"
+
+vtkStandardNewMacro(vtkMoabReader)
+//------------------------------------------------------------------------------
 vtkMoabReader::vtkMoabReader()
   {
   }
+
+//------------------------------------------------------------------------------
+vtkMoabReader::~vtkMoabReader()
+  {
+  }
+
+//------------------------------------------------------------------------------
+int vtkMoabReader::RequestInformation(vtkInformation *vtkNotUsed(request),
+                       vtkInformationVector **vtkNotUsed(inputVector),
+                       vtkInformationVector *vtkNotUsed(outputVector))
+{
+
+  //todo. Walk the file and display all the 2d and 3d elements that the users
+  //could possibly want to load
+  return 1;
+}
+
+//------------------------------------------------------------------------------
+int vtkMoabReader::RequestData(vtkInformation *vtkNotUsed(request),
+                vtkInformationVector **vtkNotUsed(inputVector),
+                vtkInformationVector *vtkNotUsed(outputVector))
+{
+  //walk the subset the user selected and read only those in
+  return 1;
+}
+
+//------------------------------------------------------------------------------
+void vtkMoabReader::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
+
