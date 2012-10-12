87bb80a4982d775315f11ecbab4d445af490bdc3
Robert Maynard <robert.maynard@kitware.com>
2012-10-12 17:26:17 -0400

    More work on connecting volumes to surfaces.

--- DumpTags/main.cxx
+++ DumpTags/main.cxx
@@ -1,5 +1,5 @@
 
-#include "tags.h"
+#include "SimpleMoab.h"
 #include <memory>
 #include <iostream>
 
@@ -12,15 +12,10 @@ int main(int argc, char **argv)
     return 0;
     }
 
-  Interface interface;
-  bool loaded = LoadMoab(argv[argc-1],interface);
-  if(!loaded)
-    {
-    std::cout << "Failed to load the file. " << std::endl;
-    return 0;
-    }
+  std::string filename(argv[argc-1]);
+  smoab::Interface interface(filename);
 
-  moab::EntityHandle rootHandle = interface->get_root_set();
+  moab::EntityHandle rootHandle = interface.getRoot();
 
   int inputOption=0;
   while(true)
@@ -28,108 +23,99 @@ int main(int argc, char **argv)
     std::cout << "What would you like to dump: " << std::endl
               << "\t1: Material and Boundary Info" << std::endl
               << "\t2: All entity sets " << std::endl
-              << "\t3: List all Groups " << std::endl
-              << "\t4: List all 3D entities " << std::endl
-              << "\t5: List all 2D entities " << std::endl
-              << "\t6: Find Root Parents" << std::endl
-              << "\t7: Find Geom Entities with multiple parents" << std::endl
-              << "\t8: Find Single Entity" << std::endl;
+              << "\t3: List all 3D entities " << std::endl
+              << "\t4: List all 2D entities " << std::endl
+              << "\t5: Find Root Parents" << std::endl
+              << "\t6: Find Detached Entities" << std::endl
+              << "\t8: Intersect Surfaces and Volume faces" << std::endl;
 
 
 
-//    std::cin >> inputOption;
-    inputOption = 2;
+    std::cin >> inputOption;
 
     moab::Range range, range2, range3;
     size_t sizes[3];
+    smoab::Range r;
     switch(inputOption)
       {
       case 1:
         std::cout << "Find all Material Set items" << std::endl;
-        findEntitiesWithTag(MaterialTag(interface),interface,rootHandle,range);
-        printRange(range,interface);
+        r = interface.findEntitiesWithTag(smoab::MaterialTag(), rootHandle);
+        interface.printRange(r);
 
         std::cout << "Find all Dirichlet Set items" << std::endl;
-        findEntitiesWithTag(DirichletTag(interface),interface, rootHandle,range);
-        printRange(range,interface);
+        r = interface.findEntitiesWithTag(smoab::DirichletTag(), rootHandle);
+        interface.printRange(r);
 
         std::cout << "Find all Neumann Set items" << std::endl;
-        findEntitiesWithTag(NeumannTag(interface),interface, rootHandle,range);
-        printRange(range,interface);
+        r = interface.findEntitiesWithTag(smoab::NeumannTag(), rootHandle);
+        interface.printRange(r);
         break;
       case 2:
-        std::cout << "Raw Sets" << std::endl;
-        interface->get_entities_by_type(rootHandle,
-                                        moab::MBTRI,
-                                        range);
-        printRange(range,interface);
-exit(1);
+        std::cout << "Entity Sets" << std::endl;
+        r = interface.findEntities(rootHandle, moab::MBENTITYSET);
+        interface.printRange(r);
         break;
       case 3:
-        std::cout << "All Groups " << std::endl;
-        findEntitiesWithTag(GroupTag(interface),interface, rootHandle, range);
-        printRange(range,interface);
+        std::cout << "3D Geom " << std::endl;
+        r = interface.findEntitiesWithTag(smoab::GeomTag(3), rootHandle);
+        interface.printRange(r);
         break;
       case 4:
-        std::cout << "3D Geom " << std::endl;
-        findEntitiesWithTag(GeomTag(interface,3),interface, rootHandle,range);
-        printRange(range,interface);
+        std::cout << "2D Geom " << std::endl;
+        r = interface.findEntitiesWithTag(smoab::GeomTag(2), rootHandle);
+        interface.printRange(r);
         break;
       case 5:
-        std::cout << "2D Geom " << std::endl;
-        findEntitiesWithTag(GeomTag(interface,2),interface, rootHandle, range);
-        printRange(range,interface);
+        std::cout << "All Entity 'Root' Parents" << std::endl;
+        r = interface.findEntityRootParents(rootHandle);
+        interface.printRange(r);
         break;
       case 6:
-        std::cout << "All parents with atleast one child" << std::endl;
-        findParents(interface,rootHandle,range);
-        printRange(range,interface);
-        break;
-      case 7:
-
-        findParents(interface,rootHandle,range);
-        findEntitiesWithMultipleParents(interface,range,range2);
-
-        range.clear();
-        findEntitiesWithTag(GeomTag(interface,1),interface, rootHandle, range);
-        range3 = moab::intersect(range,range2);
-
-        std::cout << "find 1d Entities with 2 parents" <<std::endl;
-        sizes[0] = printRange(range3,interface);
-
-        range.clear();
-        findEntitiesWithTag(GeomTag(interface,2),interface, rootHandle, range);
-        range3 = moab::intersect(range,range2);
-
-
-        std::cout << "find 2d Entities with 2 parents" <<std::endl;
-        sizes[1] = printRange(range3,interface);
-
-        range.clear();
-        findEntitiesWithTag(GeomTag(interface,3),interface, rootHandle, range);
-        range3 = moab::intersect(range,range2);
-
-
-        std::cout << "find 3d Entities with 2 parents" <<std::endl;
-        sizes[2] = printRange(range3,interface);
-
-
-        std::cout << "num 1d 2 parent elements: " << sizes[0] << std::endl;
-        std::cout << "num 2d 2 parent elements: " << sizes[1] << std::endl;
-        std::cout << "num 3d 2 parent elements: " << sizes[2] << std::endl;
+        std::cout << "All Detached Entity Sets" << std::endl;
+        r = interface.findDetachedEntities(rootHandle);
+        interface.printRange(r);
         break;
       case 8:
-        std::cout << "Input entity Id" << std::endl;
+        {
+        smoab::Range geomEnts = interface.findEntitiesWithTag(smoab::GeomTag(3), rootHandle);
+        smoab::Range parents = interface.findEntityRootParents(rootHandle);
 
-        moab::EntityHandle IdHandle;
-        std::cin >> IdHandle;
-        interface->list_entity(IdHandle);
+        smoab::Range subset = smoab::intersect(geomEnts,parents);
+        interface.printRange(subset);
 
+        typedef smoab::Range::const_iterator Iterator;
+        for(Iterator i=subset.begin();
+            i != subset.end();
+            ++i)
+          {
+          //look for all vertex indices in the hex model.
+          //than lets look and see if they are contained in the surface entity
+          //sets ever.
+
+          //we need to look at Adjacencies of the cell as that holds
+          //the face and edge!!!!!! when it has a matching surface face
+          //aka
+          //entity id: 10376293541461622798
+          // Global id = 14
+          // Adjacencies:
+          //  Vertex 80, Vertex 70, Vertex 81, Vertex 86, Vertex 270, Vertex 262, Vertex 271, Vertex 276
+          //  Edge 29, Edge 32, Edge 46, Edge 48
+          //  Quad 14
+          smoab::Range geom1Ents = interface.findEntities(*i, moab::MBHEX);
+          std::vector<smoab::EntityHandle> pointIds;
+          pointIds.reserve(geom1Ents.size() * 8);
+
+          interface.getCoordinates()
+
+          interface.printRange(geom1Ents);
+          }
+        }
         break;
       default:
+        exit(1);
         break;
       };
-    //exit(1);
     }
   return 1;
   }
