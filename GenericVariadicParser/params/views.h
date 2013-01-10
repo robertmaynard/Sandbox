fe5ab0fe1d5ea0b7d3a34e8f40443f3204f32d75
Robert Maynard <robert.maynard@kitware.com>
2013-01-10 11:04:10 -0500

    Update views.h to use new join api which works around gcc 4.6 bugs.

--- GenericVariadicParser/params/views.h
+++ GenericVariadicParser/params/views.h
@@ -1,17 +1,16 @@
 #ifndef __params_detail_views_h
 #define __params_detail_views_h
 
+//only need to include join on gcc 4.6
+#if defined(VARIADIC_JOIN_REQUIRED)
+# include "join.h"
+#endif
+
 #include <boost/mpl/vector_c.hpp>
 #include <boost/fusion/view/nview.hpp>
 
 namespace params  {
 namespace detail {
-
-  //pre GCC 4.7 has a bug with template expansion into
-  //non-variadic class template (aka base case).
-  //see gcc bug 35722, for the workaround I am using.
-  template< template <class, long ...> class T, long... Args>
-  struct L_Join { typedef T<int,Args...> type; };
 
   //generate a mpl vector of incrementing values from Start to End
   //which is inclusive at the start, exclusive at end.
@@ -28,7 +27,11 @@ namespace detail {
   template<class Sequence, int Start, int ...Indices>
   struct make_nview<Sequence,Start,Start,Indices...>
     {
-    typedef typename L_Join<boost::mpl::vector_c,Indices...>::type itemIndices;
+#if defined(VARIADIC_JOIN_REQUIRED)
+    typedef typename params::detail::l_join<boost::mpl::vector_c,Indices...>::type itemIndices;
+#else
+    typedef boost::mpl::vector_c<int,Indices...> itemIndices;
+#endif
     typedef ::boost::fusion::nview<Sequence,itemIndices> type;
     };
 
