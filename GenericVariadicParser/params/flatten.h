8e545c1502c6cce59737b67e87d26aa5ae55e4c5
Robert Maynard <robert.maynard@kitware.com>
2013-01-10 11:01:52 -0500

    flatten supports variadic macros, and move details to different header.
    no reason to have all of the flatten implementation details being wrapped
    in lots of boost preprocessor ifdefs, so move to separate header

--- GenericVariadicParser/params/flatten.h
+++ GenericVariadicParser/params/flatten.h
@@ -1,117 +1,15 @@
+#ifndef BOOST_PP_IS_ITERATING
+
 #ifndef __params_flatten_h
 #define __params_flatten_h
 
 #include "common.h"
+#include "flatten_detail.h"
 
-#include <boost/fusion/algorithm/transformation/insert_range.hpp>
-#include <boost/fusion/algorithm/transformation/push_back.hpp>
-#include <boost/fusion/functional/invocation/invoke_procedure.hpp>
-#include <boost/fusion/iterator/deref.hpp>
-#include <boost/fusion/iterator/next.hpp>
-#include <boost/fusion/support/is_sequence.hpp>
-#include <boost/fusion/support/is_view.hpp>
-#include <boost/mpl/or.hpp>
-#include <boost/type_traits/remove_reference.hpp>
-
-//work around a bug in boost::fusion::is_view in that it
-//doesn't properly implement the non_fusion_tag as meaning something isn't
-//a view
-namespace boost { namespace fusion { namespace extension {
-  template <>
-  struct is_view_impl<non_fusion_tag>
-  {
-      template <typename Sequence>
-      struct apply : mpl::false_ {};
-  };
-}}}
-
-namespace params {
-  namespace detail {
-
-  namespace fusion = ::boost::fusion;
-
-  template<class I>
-  struct is_fusion_sequence
-  {
-    //deref I as it is an iterator
-    typedef typename fusion::result_of::deref<I>::type derefType;
-
-    //the iterator might be to a sequence or view reference, which
-    //doesn't compile, so we have to remove the reference
-    typedef typename boost::remove_reference<derefType>::type deducedType;
-
-    //check if it is a sequence or view
-    typedef typename fusion::traits::is_sequence<deducedType>::type isSequenceType;
-    typedef typename fusion::traits::is_view<deducedType>::type isViewType;
-    typedef typename boost::mpl::or_< isSequenceType, isViewType >::type IsIterator;
-  public:
-    typedef IsIterator type;
-  };
-
-
-  template< int Size, int Element, class Functor>
-  struct flatten_impl
-  {
-    template<class Item, class Sequence>
-    void operator()(Functor& f, Item item, Sequence s,
-                    typename boost::enable_if<
-                    typename params::detail::is_fusion_sequence<Item>::type >::type* = 0) const
-    {
-      //insert every element in the sequence contained in item to the end
-      //of the Sequence s.
-      flatten_impl<Size,Element+1,Functor>()(f,
-                 fusion::next(item),
-                 fusion::insert_range(s,fusion::end(s),fusion::deref(item)));
-    }
-
-    template<class Item, class Sequence>
-    void operator()(Functor& f, Item item, Sequence s,
-                    typename boost::disable_if<
-                    typename params::detail::is_fusion_sequence<Item>::type >::type* = 0) const
-    {
-      //push the derefence of the item onto the end of sequence s
-      flatten_impl<Size,Element+1,Functor>()(f,
-                                   fusion::next(item),
-                                   fusion::push_back(s,fusion::deref(item)));
-    }
-  };
-
-  //termination case of the flatten recursive calls
-  template< int Size, class Functor>
-  struct flatten_impl<Size, Size, Functor>
-  {
-    template<class Item, class Sequence>
-    void operator()(Functor& f, Item, Sequence s) const
-    {
-      //item is an iterator pointing to end, so everything is in sequence
-    fusion::invoke_procedure(f,s);
-    }
-  };
-
-  //function that starts the flatten recursion when the first item is a sequence
-  template< int Size, class Functor, class Item>
-  void flatten(Functor& f, Item item,
-               typename boost::enable_if<
-               typename params::detail::is_fusion_sequence<Item>::type >::type* = 0)
-  {
-    params::detail::flatten_impl<Size,1,Functor>()(f,fusion::next(item),
-                                                     fusion::deref(item));
-  }
-
-  //function that starts the flatten recursion when the first item isn't a sequence
-  template< int Size, class Functor, class Item>
-  void flatten(Functor& f, Item item,
-               typename boost::disable_if<
-               typename params::detail::is_fusion_sequence<Item>::type >::type* = 0)
-  {
-    typedef typename fusion::result_of::deref<Item>::type ItemDeRef;
-    typedef fusion::single_view<ItemDeRef> ItemView;
-    ItemView iv(fusion::deref(item));
-    params::detail::flatten_impl<Size,1,Functor>()(f,fusion::next(item),iv);
-  }
-
-} } //params::detail
-
+///////////////////////////////////////////////////////////////////////////////
+// Variadic Implementation
+///////////////////////////////////////////////////////////////////////////////
+#if defined(VARIADIC_SUPPORT)
 
 namespace params
 {
@@ -121,7 +19,7 @@ namespace params
             class ... Args>
   void flatten(Functor& f, Args... args)
   {
-    typedef typename ::params::Join< ::params::vector,Args... >::type Sequence;
+    typedef typename ::params::vector_type< Args... >::type Sequence;
     Sequence all_args(args...);
 
     ::params::detail::flatten<
@@ -130,4 +28,34 @@ namespace params
   }
 
 }
-#endif
+
+#else //VARIADIC_SUPPORT
+# define BOOST_PP_ITERATION_PARAMS_1 (3, (2, FUSION_MAX_VECTOR_SIZE,"params/flatten.h"))
+# include BOOST_PP_ITERATE()
+#endif //VARIADIC_SUPPORT
+
+
+#endif //__params_flatten_h
+
+///////////////////////////////////////////////////////////////////////////////
+//  Preprocessor vertical repetition code
+///////////////////////////////////////////////////////////////////////////////
+#else // defined(BOOST_PP_IS_ITERATING)
+
+namespace params
+{
+  //take an arbitrary class that has a parameter pack and flatten it so
+  //that we can call a method with each element of the class
+  template< class Functor, __pp_class_Args__>
+  void flatten(Functor& f, __pp_params_Args__(args) )
+  {
+    typedef typename ::params::vector_type< __pp_Args__ >::type Sequence;
+    Sequence all_args(__pp_values_Args__(args));
+
+    ::params::detail::flatten<
+      ::params::detail::num_elements<Sequence>::value,
+      Functor>(f,boost::fusion::begin(all_args));
+  }
+
+}
+#endif //BOOST_PP_IS_ITERATING
---
+++ GenericVariadicParser/params/flatten_detail.h
@@ -0,0 +1,113 @@
+#ifndef __params_detail_flatten_h
+#define __params_detail_flatten_h
+
+#include <boost/fusion/algorithm/transformation/insert_range.hpp>
+#include <boost/fusion/algorithm/transformation/push_back.hpp>
+#include <boost/fusion/functional/invocation/invoke_procedure.hpp>
+#include <boost/fusion/iterator/deref.hpp>
+#include <boost/fusion/iterator/next.hpp>
+#include <boost/fusion/support/is_sequence.hpp>
+#include <boost/fusion/support/is_view.hpp>
+#include <boost/mpl/or.hpp>
+#include <boost/type_traits/remove_reference.hpp>
+
+//work around a bug in boost::fusion::is_view in that it
+//doesn't properly implement the non_fusion_tag as meaning something isn't
+//a view
+namespace boost { namespace fusion { namespace extension {
+  template <>
+  struct is_view_impl<non_fusion_tag>
+  {
+      template <typename Sequence>
+      struct apply : mpl::false_ {};
+  };
+}}}
+
+namespace params {
+  namespace detail {
+
+  namespace fusion = ::boost::fusion;
+
+  template<class I>
+  struct is_fusion_sequence
+  {
+    //deref I as it is an iterator
+    typedef typename fusion::result_of::deref<I>::type derefType;
+
+    //the iterator might be to a sequence or view reference, which
+    //doesn't compile, so we have to remove the reference
+    typedef typename boost::remove_reference<derefType>::type deducedType;
+
+    //check if it is a sequence or view
+    typedef typename fusion::traits::is_sequence<deducedType>::type isSequenceType;
+    typedef typename fusion::traits::is_view<deducedType>::type isViewType;
+    typedef typename boost::mpl::or_< isSequenceType, isViewType >::type IsIterator;
+  public:
+    typedef IsIterator type;
+  };
+
+
+  template< int Size, int Element, class Functor>
+  struct flatten_impl
+  {
+    template<class Item, class Sequence>
+    void operator()(Functor& f, Item item, Sequence s,
+                    typename boost::enable_if<
+                    typename params::detail::is_fusion_sequence<Item>::type >::type* = 0) const
+    {
+      //insert every element in the sequence contained in item to the end
+      //of the Sequence s.
+      flatten_impl<Size,Element+1,Functor>()(f,
+                 fusion::next(item),
+                 fusion::insert_range(s,fusion::end(s),fusion::deref(item)));
+    }
+
+    template<class Item, class Sequence>
+    void operator()(Functor& f, Item item, Sequence s,
+                    typename boost::disable_if<
+                    typename params::detail::is_fusion_sequence<Item>::type >::type* = 0) const
+    {
+      //push the derefence of the item onto the end of sequence s
+      flatten_impl<Size,Element+1,Functor>()(f,
+                                   fusion::next(item),
+                                   fusion::push_back(s,fusion::deref(item)));
+    }
+  };
+
+  //termination case of the flatten recursive calls
+  template< int Size, class Functor>
+  struct flatten_impl<Size, Size, Functor>
+  {
+    template<class Item, class Sequence>
+    void operator()(Functor& f, Item, Sequence s) const
+    {
+      //item is an iterator pointing to end, so everything is in sequence
+    fusion::invoke_procedure(f,s);
+    }
+  };
+
+  //function that starts the flatten recursion when the first item is a sequence
+  template< int Size, class Functor, class Item>
+  void flatten(Functor& f, Item item,
+               typename boost::enable_if<
+               typename params::detail::is_fusion_sequence<Item>::type >::type* = 0)
+  {
+    params::detail::flatten_impl<Size,1,Functor>()(f,fusion::next(item),
+                                                     fusion::deref(item));
+  }
+
+  //function that starts the flatten recursion when the first item isn't a sequence
+  template< int Size, class Functor, class Item>
+  void flatten(Functor& f, Item item,
+               typename boost::disable_if<
+               typename params::detail::is_fusion_sequence<Item>::type >::type* = 0)
+  {
+    typedef typename fusion::result_of::deref<Item>::type ItemDeRef;
+    typedef fusion::single_view<ItemDeRef> ItemView;
+    ItemView iv(fusion::deref(item));
+    params::detail::flatten_impl<Size,1,Functor>()(f,fusion::next(item),iv);
+  }
+
+} } //params::detail
+
+#endif
