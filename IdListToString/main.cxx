f669ead45a099cf379d94a8ad4a37ef8bdd09d9d
Robert Maynard <robert.maynard@kitware.com>
2013-04-03 12:12:01 -0400

    More robust test to verify the algorithm works on more ids lists.

--- IdListToString/main.cxx
+++ IdListToString/main.cxx
@@ -2,39 +2,67 @@
 #include <vtkNew.h>
 #include "convert.h"
 
+#include <sys/time.h>
+
+#include <algorithm>
+#include <numeric>
+
+void random_fill(vtkIdList* list, int max_count, int max_value)
+{
+  //srand on time
+  srand (time(NULL));
+
+  //start random seed
+  timeval currentTime;
+  gettimeofday(&currentTime, NULL);
+
+  vtkIdType size = 1 + ((int)currentTime.tv_usec % (max_count-1) );
+  list->SetNumberOfIds(size);
+
+  for(int i=0; i < size; ++i)
+  {
+    list->SetId(i,(int)currentTime.tv_usec % max_value);
+  }
+
+}
+
+void reverseList(vtkIdList* in, vtkIdList* out)
+{
+  out->DeepCopy(in);
+  vtkIdType size = out->GetNumberOfIds();
+  std::reverse(out->GetPointer(0),out->GetPointer(size));
+}
 int main(int, char**)
 {
-  vtkNew<vtkIdList> descending;
-  descending->SetNumberOfIds(3);
-  descending->SetId(0,5);
-  descending->SetId(1,4);
-  descending->SetId(2,3);
+  //we will test 1000 random list of values each with any where from
+  //1 to 10,000 values. the generated values will be between 0 and 1million
+  for(int i=0; i < 1000; ++i)
+  {
+    vtkNew<vtkIdList> listToTest;
+    //10000 max value size, 1mill max id value
+    random_fill(listToTest.GetPointer(),10000,1000000);
 
-  std::string desc = to_key(descending.GetPointer());
+    std::string result = to_key(listToTest.GetPointer());
 
-  if(desc != to_key(descending->GetPointer(0),3) )
+    if(result != to_key(listToTest->GetPointer(0),listToTest->GetNumberOfIds()) )
     {
-    std::cerr << "key's failed to match given same input sequence past by pointer"  << std::endl;
+    std::cerr << "key's failed to match when past by pointer"  << std::endl;
     return 1;
     }
 
-  std::cout << "descending key is: " << desc << std::endl;
+    vtkNew<vtkIdList> reversedList;
+    reverseList(listToTest.GetPointer(),reversedList.GetPointer());
+    std::string reversedResult = to_key(reversedList.GetPointer());
 
-  vtkNew<vtkIdList> ascending;
-  ascending->SetNumberOfIds(3);
-  ascending->SetId(0,3);
-  ascending->SetId(1,4);
-  ascending->SetId(2,5);
-
-  std::string asc = to_key(ascending.GetPointer());
-
-  if(desc != asc )
+    if(result != reversedResult )
     {
-    std::cerr << "key's failed to match given ascending and descending order" << std::endl;
+    std::cerr << "key's failed to match given reversed order" << std::endl;
     return 1;
     }
 
-  std::cout << "descending key is: " << asc << std::endl;
+    std::cout << "gen key: " << result << " len: " <<  listToTest->GetNumberOfIds() << std::endl;
+  }
 
-  return 0;
+return 0;
 }
+
