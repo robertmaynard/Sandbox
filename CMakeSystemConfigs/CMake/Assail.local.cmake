9a2284f832fc37c977a38a0154afb478a1aa8e30
Robert Maynard <robert.maynard@kitware.com>
2013-09-18 22:09:36 -0400

    Show how you can use system introspection to include system config files.

---
+++ CMakeSystemConfigs/CMake/Assail.local.cmake
@@ -0,0 +1,9 @@
+
+set(MPI_CXX_FOUND           TRUE)
+set(MPI_CXX_COMPILER        "/usr/bin/mpicxx")
+set(MPI_CXX_COMPILE_FLAGS   "")
+set(MPI_CXX_INCLUDE_PATH    "/usr/include/local")
+set(MPI_CXX_LINK_FLAGS      "")
+set(MPI_CXX_LIBRARIES       "")
+
+message("Included the Assail configurations")
---
+++ CMakeSystemConfigs/README.md
@@ -0,0 +1,1 @@
+Show how to setup and use system specific configuration files.
