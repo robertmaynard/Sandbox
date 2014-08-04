4e90106bf86c891831611a48415a08203250949b
Robert Maynard <robert.maynard@kitware.com>
2014-08-04 14:57:37 -0400

    More changes to verify link to zlib

--- CudaSharedLibLinking/foo.cu
+++ CudaSharedLibLinking/foo.cu
@@ -1,4 +1,6 @@
 #include "cuda.h"
+#include "zlib.h"
+
 #include <iostream>
 
 #include "foo.h"
@@ -11,4 +13,12 @@ Foo::Foo()
 
   std::cout << "device count" << num << std::endl;
   std::cout << "device id" << devid << std::endl;
+
+  int level=1;
+  z_stream strm;
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.opaque = Z_NULL;
+  int ret = deflateInit(&strm, level);
+  std::cout << "deflateInit ret :" << ret << std::endl;
   }
--- CudaSharedLibLinking/main.cxx
+++ CudaSharedLibLinking/main.cxx
@@ -1,4 +1,3 @@
-#include "zlib.h"
 
 #include "foo.h"
 
