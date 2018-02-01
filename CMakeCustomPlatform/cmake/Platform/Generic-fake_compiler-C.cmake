253f91f42ced931c63d967d7fab9ba8e9c9cdd02
Robert Maynard <robert.maynard@kitware.com>
2018-02-01 11:31:33 -0500

    add a platform file for the fake_compiler C

---
+++ CMakeCustomPlatform/cmake/Platform/Generic-fake_compiler-C.cmake
@@ -0,0 +1,5 @@
+
+#This file just needs to specify non default values
+set(CMAKE_INCLUDE_FLAG_C "--include=")
+
+set(CMAKE_C_COMPILE_OBJECT  "<CMAKE_C_COMPILER> --compile=<SOURCE> <DEFINES> <INCLUDES> <FLAGS> --output=<OBJECT>")
